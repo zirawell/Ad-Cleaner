ea6f26a0371c9150fda1cf7327c269b0126ddbe1
Dark Wang <zirawell@gmail.com>
2024-07-23 08:36:20 +0800

    update mafengwo

---
+++ Collection/js/mafengwo.js
@@ -0,0 +1,44 @@
+/********************************
+Mafengwo Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+^https?:\/\/mapi\.mafengwo\.cn\/user\/profile\/get_list url script-response-body https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/mafengwo.js
+^https?:\/\/mapi\.mafengwo\.cn\/rest\/app\/daka\/index url script-response-body https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/mafengwo.js
+
+Please note that the above rewrite link requires open KOP-XIAO's resource parser
+
+*********************************
+Surge4, Loon and Shadowrocket configuration:
+
+[Script]
+http-response ^https?:\/\/mapi\.mafengwo\.cn\/user\/profile\/get_list script-path=https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/mafengwo.js
+http-response ^https?:\/\/mapi\.mafengwo\.cn\/rest\/app\/daka\/index script-path=https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/mafengwo.js
+
+[MITM]
+hostname = mapi.mafengwo.cn
+********************************/
+
+const url = $request.url;
+const obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
+let body = $response.body;
+
+if(obj && obj.data){
+  // 打卡提醒
+  if (url.includes("/daka/index")){
+    obj.data.has_daka = 1;
+  // 我的页推广内容
+  }else if(url.includes("/profile/get_list")){
+    if(obj?.data?.list?.length > 0){
+      obj.data.list = obj.data.list.filter(function(item) {
+          if(item.style != "inspire"){
+            return item;
+          }
+      });
+    }
+  }
+  body = JSON.stringify(obj);
+  $done({ body });
+}else{
+  $done();
+}
