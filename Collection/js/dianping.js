add055cdb16832e57c96b1a3901c795f9529e062
Dark Wang <zirawell@gmail.com>
2024-08-04 20:33:06 +0800

    update dianping

--- Collection/js/dianping.js
+++ Collection/js/dianping.js
@@ -3,7 +3,7 @@ Dianping Remove Ads - Version 1.0
 Please note that you may need to reinstall app for script to work.
 
 QuantumultX rewrite link:
-^https?:\/\/img\.meituan\.net\/dpmobile\/ url script-response-header https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/dianping.js
+^https?:\/\/img\.meituan\.net\/dpmobile\/ url script-response-body https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/dianping.js
 
 Please note that the above rewrite link requires open KOP-XIAO's resource parser
 
@@ -11,7 +11,7 @@ Please note that the above rewrite link requires open KOP-XIAO's resource parser
 Surge4, Loon and Shadowrocket configuration:
 
 [Script]
-http-request ^https?:\/\/img\.meituan\.net\/dpmobile\/ script-path=https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/dianping.js
+http-response ^https?:\/\/img\.meituan\.net\/dpmobile\/ script-path=https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/dianping.js
 
 [MITM]
 hostname = mobilepaas.abchina.com.cn
@@ -23,8 +23,5 @@ const header = $request.headers;
 const headopt = header["M-SHARK-TRACEID"] || header["m-shark-traceid"];
 
 if (headopt != null) {
-  resp.headers = $request.headers;
   $done(resp);
-} else {
-  $done({});
-}
+} 
